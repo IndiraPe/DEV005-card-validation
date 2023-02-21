8a4a8d27ad0fd16c260adc74a8e3906d4cea757c
IndiraPe <forever_tk_16@hotmail.com>
2023-02-21 13:16:19 -0500

    Avanzando la validación

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import validator from './validator.js';
+//Elementos que trabajaré
+const btn = document.getElementById('btn');
+const check1 = document.getElementById('check1');
+const check2 = document.getElementById('check2');
+const question = document.getElementById('question');
+const btn2 = document.getElementById('btn2');
 
-console.log(validator);
+//Activando el botón
+btn.addEventListener('click',validation);
+btn2.addEventListener('click',restart);
+
+//function
+function validation() {
+  const numberCard = document.getElementById('numberCard').value;
+  if(numberCard===''){
+    check1.innerHTML='Tienes que poner digitos en la caja'
+    check1.style.display = 'block';
+    // return null;
+  }
+  const final = validator.isValid(numberCard);
+
+  if (final % 10 === 0){
+    check1.style.display = 'block';
+  } else {
+    check2.style.display = 'block';
+  }
+  
+  question.style.display = 'none';
+  btn2.style.display = 'block';
+}
+function restart(){
+  window.location.reload();
+}
+
+
+//console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,36 @@
 const validator = {
-  // ...
+  isValid: function (numberCard) {
+    //Transformando el número de tarjeta
+    const arrayCard = Array.from(numberCard).reverse().map(a=>parseInt(a));
+    
+    let sumNumberPair = 0;
+    let sumNumberOdd = 0;
+    //ubicar posiciones pares y x2
+    for(let i=1; i<arrayCard.length; i=i+2){
+      const arrayCardDuple = arrayCard[i]*2;
+      //dígitos mayores de 9
+      let biggerNumber = 0;
+      let minorNumber = 0;
+      if (arrayCardDuple > 9) {
+        biggerNumber = arrayCardDuple - 9;
+      }
+      if (arrayCardDuple <= 9) {
+        minorNumber = arrayCardDuple;
+      }
+      //sumar los numeros de la posicion par
+      const bothNumber = biggerNumber + minorNumber;
+      sumNumberPair = sumNumberPair + bothNumber;
+    } 
+    //Ubicando los impares y sumarlos
+    for(let i=0; i<arrayCard.length; i=i+2){
+      const arrayCardNormal = arrayCard[i];
+      sumNumberOdd = sumNumberOdd + arrayCardNormal;
+    }
+    //sumando los pares y los impares 
+    const sumTotal = sumNumberPair + sumNumberOdd;
+    return sumTotal;
+  }
+
 };
 
 export default validator;
